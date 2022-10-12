91289d8dabbb96a9fdf85bdf06156cf12a08029b
JFrederickK <576723@student.belgiumcampus.ac.za>
2022-10-12 11:10:53 +0200

    Database connection

---
+++ SEN381_API_GROUP3/SEN381_API_GROUP3/Database/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace SEN381_API_GROUP3.Database
+{
+    public class Connection
+    {
+        public SqlConnection ConnectDatabase()
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source = .; Initial Catalog = UkupholisaHealthcare; Integrated Security = True");
+
+            return connection;
+        }
+    }
+}
