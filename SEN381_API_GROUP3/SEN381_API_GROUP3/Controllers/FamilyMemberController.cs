c90e41c32472947dab780049415828f632101b37
WernerPret <wernerpretorius333@gmail.com>
2022-10-15 17:52:57 +0200

    Implemented Family Member Service

--- SEN381_API_GROUP3/SEN381_API_GROUP3/Controllers/FamilyMemberController.cs
+++ SEN381_API_GROUP3/SEN381_API_GROUP3/Controllers/FamilyMemberController.cs
@@ -2,6 +2,7 @@
 using SEN381_API_GROUP3.Database;
 using SEN381_API_Group3.shared.models;
 using System.Data.SqlClient;
+using SEN381_API_GROUP3.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,50 +16,14 @@ namespace SEN381_API_GROUP3.Controllers
         [HttpGet]
         public List<FamilyMember> Get(int page, int size)
         {
-            int offset = (page - 1) * size;
-            List<FamilyMember> modules = new List<FamilyMember>();
-            Connection con = new Connection();
-            SqlConnection scon = con.ConnectDatabase();
-
-            SqlCommand command = new SqlCommand("SELECT * FROM [dbo].[FamilyMember] ORDER BY FamilyMemberID OFFSET @offset ROWS FETCH NEXT @size ROWS ONLY;", scon);
-            command.Parameters.AddWithValue("@offset", offset);
-            command.Parameters.AddWithValue("@size", size);
-            SqlDataReader reader = command.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    modules.Add(new FamilyMember(reader.GetInt32(0).ToString(), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetInt32(8)));
-
-
-                }
-            }
-
-
-            return modules;
+            return new FamilyMemberService().getAllFamilyMembers(page, size);
         }
 
         // GET api/<FamilyMemberController>/5
         [HttpGet("{id}")]
         public List<FamilyMember> Get(int id)
         {
-            List<FamilyMember> modules = new List<FamilyMember>();
-            Connection con = new Connection();
-            SqlConnection scon = con.ConnectDatabase();
-            SqlCommand command = new SqlCommand($@"SELECT * FROM [dbo].[FamilyMember] where FamilyMemberID = '{id}'", scon);
-            SqlDataReader reader = command.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    modules.Add(new FamilyMember(reader.GetInt32(0).ToString(), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetInt32(8)));
-                }
-            }
-
-
-            return modules;
+            return new FamilyMemberService().getFamilyMemerById(id);
         }
 
         // POST api/<FamilyMemberController>
