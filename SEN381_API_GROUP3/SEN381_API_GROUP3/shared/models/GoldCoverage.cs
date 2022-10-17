587146d310dbe90a168903f9716347c2020a44a1
DeRaven <stoltz278@gmail.com>
2022-10-17 21:25:55 +0200

    .

--- SEN381_API_GROUP3/SEN381_API_GROUP3/shared/models/GoldCoverage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SEN381_API_Group3.shared.models;
-
-public class GoldCoverage : TreatmentCoverage
-{
-    public GoldCoverage(int coverageID, string coverageDescription, int numberOfGeneralVisits, int numberOfSpecialistsVisits, int totalCoverageUser) : base(coverageID, coverageDescription, numberOfGeneralVisits, numberOfSpecialistsVisits, totalCoverageUser)
-    {
-    }
-
-}
--- SEN381_API_GROUP3/SEN381_API_GROUP3/shared/models/SilverCoverage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SEN381_API_Group3.shared.models;
-
-public class SilverCoverage : TreatmentCoverage
-{
-    public SilverCoverage(int coverageID, string coverageDescription, int numberOfGeneralVisits, int numberOfSpecialistsVisits, int totalCoverageUser) : base(coverageID, coverageDescription, numberOfGeneralVisits, numberOfSpecialistsVisits, totalCoverageUser)
-    {
-    }
-
-}
